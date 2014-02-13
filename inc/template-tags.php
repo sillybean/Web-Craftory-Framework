55c2cbc978d50483224bbad378e4b419a6119b01
Stephanie Leary <steph@sillybean.net>
2014-02-13 14:24:07 -0600

    Better wording for next/previous post link in search results

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -67,12 +67,22 @@ function craftory_content_nav( $nav_id ) {
 
 	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
 
+		<?php 
+		if ( is_search() ) {
+			$next = __( '<span class="meta-nav">&larr;</span> More results', 'craftory' );
+			$previous = __( 'Previous results <span class="meta-nav">&rarr;</span>', 'craftory' );
+		}
+		else {
+			$next = __( '<span class="meta-nav">&larr;</span> Older posts', 'craftory' );
+			$previous = __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'craftory' );
+		} 
+		?>
 		<?php if ( get_next_posts_link() ) : ?>
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'craftory' ) ); ?></div>
+		<div class="nav-previous"><?php next_posts_link( $next ); ?></div>
 		<?php endif; ?>
 
 		<?php if ( get_previous_posts_link() ) : ?>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'craftory' ) ); ?></div>
+		<div class="nav-next"><?php previous_posts_link( $previous ); ?></div>
 		<?php endif; ?>
 
 	<?php endif; ?>
